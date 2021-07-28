cb50b12ef7acec34ddab6d71e573e03935a5432f
DiegoCoriolano <cdiego.dm@hotmail.com>
2021-07-28 20:37:17 -0300

    JavaScript adicionado
    Adicionado o início do JavaScript.
    Ainda buscando as soluções para resolver os problemas

--- js/script.js
+++ js/script.js
@@ -1,15 +1,48 @@
 function calc(){
-    var bill = document.getElementById("value-box");
-    var npeople = document.getElementById("npeople");
+    var bill = document.getElementById("value-box").value;
+    var npeople = document.getElementById("npeople").value;
+
+    var tip = document.getElementsByName("tip-select");
+
+    var tipBox = documentgetElementById("custom-tip");
+
+    if (tipBox=""){
+        for (var i=0; i<=tip.length-1; i++){
+            if (tip[i].checked==true){
+                var tipValue=tip.value;
+            }
+        } else{
+            
+        }
+
+
+    }
+
+    
+    
 
 
     if (bill!="" && npeople!=""){
-    var TipAmount = (bill.value/npeople.value);
-    document.getElementById("tip-amount-value").innerText=TipAmount;
+    var TipAmount = (bill/npeople);
+    document.getElementById("tip-amount-value").innerText=TipAmount.toFixed(2);
     }
 
 }
 
+function customTip(){
+
+        var tip05 = document.getElementById("tip05");
+        tip05.checked=false;
+        var tip10 = document.getElementById("tip10");
+        tip10.checked=false;
+        var tip15 = document.getElementById("tip15");
+        tip15.checked=false;
+        var tip25 = document.getElementById("tip25");
+        tip25.checked=false;
+        var tip50 = document.getElementById("tip50");
+        tip50.checked=false;
+        
+}
 
 function reset(value){
     var valuebox = document.getElementById("value-box");
