1bb36b6eb3ebf3f152968ebc9ce45359e65ce3f3
DiegoCoriolano <cdiego.dm@hotmail.com>
2021-07-29 11:17:58 -0300

    JavaScript atualizado
    Caso o valor do Tip esteja em custom, todo o cálculo será feito!
    
    • Adicionado um campo além do desafio. Campo de total do Tip, para mostrar o valor sem dividir por pessoa

--- js/script.js
+++ js/script.js
@@ -1,33 +1,38 @@
 function calc(){
     var bill = document.getElementById("value-box").value;
-    var npeople = document.getElementById("npeople").value;
+    var npeople =document.getElementById("npeople").value;
 
     var tip = document.getElementsByName("tip-select");
 
-    var tipBox = documentgetElementById("custom-tip");
-
-    if (tipBox=""){
-        for (var i=0; i<=tip.length-1; i++){
-            if (tip[i].checked==true){
-                var tipValue=tip.value;
-            }
-        } else{
-            
-        }
-
-
+    var tipBox = document.getElementById("custom-tip").value;
+    if (npeople==0){
+        
     }
+    if (bill!="" && npeople!="" && tipBox!=""){
 
-    
-    
+        var tip05 = document.getElementById("tip05");
+        tip05.checked=false;
+        var tip10 = document.getElementById("tip10");
+        tip10.checked=false;
+        var tip15 = document.getElementById("tip15");
+        tip15.checked=false;
+        var tip25 = document.getElementById("tip25");
+        tip25.checked=false;
+        var tip50 = document.getElementById("tip50");
+        tip50.checked=false;
 
+        var tipAmount = ((bill*(tipBox/100)/npeople));
+        document.getElementById("tip-amount-value").innerText = "$" + tipAmount.toFixed(2);
 
-    if (bill!="" && npeople!=""){
-    var TipAmount = (bill/npeople);
-    document.getElementById("tip-amount-value").innerText=TipAmount.toFixed(2);
+        var tipTotal = ((parseFloat(bill)+parseFloat(tipAmount))/npeople);
+        document.getElementById("tip-total-value").innerText = "$" + tipTotal.toFixed(2);
+
+        var tipAmountTotal = ((parseFloat(bill)*parseFloat(tipBox/100)));
+        document.getElementById("tip-total").innerText = "$" + tipAmountTotal.toFixed(2);
     }
 
 }
+
 
 function customTip(){
 
@@ -62,4 +67,10 @@ function reset(value){
     custom.value="";
 
     document.getElementById("npeople").value="";
+
+    document.getElementById("tip-amount-value").innerText = "$0,00";
+
+    document.getElementById("tip-total-value").innerText = "$0,00";
+
+    document.getElementById("tip-total").innerText = "$0,00";
 }
